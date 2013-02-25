e9abdc08c8d5e281f61f48eef51cc0f0f095c2a9
pivotal <pivotal@managers.(none)>
2013-02-25 13:03:04 -0700

    Add GPIO gem and embed iframe for webcam.

--- app/door_buzzer_server.rb
+++ app/door_buzzer_server.rb
@@ -1,5 +1,11 @@
 require 'sinatra'
+require 'gpio'
 
+#TODO: parse webcam url from config.yml
 get '/' do
+  haml :index
+end
 
+get '/buzz' do
+  GPIO::Pin.new(pin: 22)
 end
--- spec/door_buzzer_server_spec.rb
+++ spec/door_buzzer_server_spec.rb
@@ -2,6 +2,5 @@ require 'spec_helper'
 
 describe 'The most awesomest door buzzing app EVAAARRRRR' do
   it 'should buzz me in' do
-    
   end
 end
